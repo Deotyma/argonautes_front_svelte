3a23f65098f732dcbcb645f7609e1329e5d51367
Ewa Kadziolka <kalistaewa@gmail.com>
2022-06-28 08:04:15 +0200

    add paralax to home page

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -8,39 +8,74 @@
      import tresor from "../assets/images/tresor.png"
      import amour from "../assets/images/amour.png";
      import rejoint from "../assets/images/regoint.png";
+     import { onMount } from "svelte";
+     import simpleParallax from 'simple-parallax-js'
+
+     onMount(()=>{
+         const image1 = document.getElementsByClassName("img1");
+            new simpleParallax(image1,{
+                scale: 1.5,
+                delay: 0.5,
+                orientation: "down"
+            });
+
+            const image2 = document.getElementsByClassName("img2");
+            new simpleParallax(image2,{
+                scale: 0.8,
+                delay: 0.8,
+                orientation:'right'
+            });
+
+            const image3 = document.getElementsByClassName("img3");
+            new simpleParallax(image3,{
+                scale: 0.8,
+                delay: 0.8,
+                orientation:'left'
+            });
+        }
+     )
 </script>
 
 <main>
     <!-- section 1 -->
     <div class="section">
         <img class="img1" src={grece1} alt="grece"/>
-        <p class="text1">Un voyage magnifique t'attand...</p>
-        <img class="img2"  src={voyage} alt="voyage"/>
+        <div>
+            <p class="text1">Un voyage magnifique t'attand...</p>
+            <img class="img2"  src={voyage} alt="voyage"/>
+        </div>
     </div>
     <!--section 2  -->
     <div class="section">
-        <img class="img3" src={tresor} alt="tresor"/>
-        <p class="text2">Un trésor inestimable reien que pour toi...</p>
-        <img class="img4"  src={grece2} alt="ruines">
+        <div>
+            <img class="img3" src={tresor} alt="tresor"/>
+            <p class="text2">Un trésor inestimable reien que pour toi...</p>
+        </div>    
+        <img class="img1"  src={grece2} alt="ruines">
     </div>
     <!-- section 3 -->
     <div class="section">
-        <img class="img5" src={grece3} alt="grece">
-        <p class="text3">Un amour pour la vie et pour la mort...</p>
-        <img class="img6"  src={amour} alt="amour">
-
+        <img class="img1" src={grece3} alt="grece">
+        <div>
+            <p class="text3">Un amour pour la vie et pour la mort...</p>
+            <img class="img2"  src={amour} alt="amour">
+        </div>
     </div>
     <!-- section 4 -->
     <div class="section">
-        <img class="img7"  src={rejoint} alt="rejoint nous">
-        <a class="text4"href="/register" use:link>Réjoint notre aventure...</a>
-        <img class="img8" src={grece4} alt="ruines">
+        <div>
+            <img class="img3"  src={rejoint} alt="rejoint nous">
+            <a class="text4" href="/register" use:link>Réjoint notre aventure...</a>
+        </div>
+        <img class="img1" src={grece4} alt="ruines">
     </div>
 </main>
 
 <style>
     .section{
         display: flex;
+        align-items: center;
+        padding: 2rem;
     }
     img{
         height:auto;
@@ -49,16 +84,14 @@
 
     }
 
-    .img1, .img4, .img5, .img8{
+    .img1{
         box-shadow: 10px 10px 5px #2E445A;
     }
 
-    .text1{
-        position: relative;
-        right: 200px;
-        bottom: 100px;
-        font-size: 25px;
-        color: black;
+    .text1, .text2, .text3, .text4 {
+        font-size: 35px;
+        color: #2E445A;
+        text-align: center;
     }
 
 </style>
